544f2d3fed4d176e2bf1b2eca56a8d78d75556d4
Raihanmiraj <65871430+Raihanmiraj@users.noreply.github.com>
2022-02-11 01:22:46 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,8 @@ import * as serviceWorker from './serviceWorker';
 import axios from 'axios';
 
 // axios.defaults.baseURL = "http://127.0.0.1:8000/api";
-axios.defaults.baseURL = "http://localhost/reactapi/public/api";
+// axios.defaults.baseURL = "http://localhost/reactapi/public/api";
+axios.defaults.baseURL = "https://aloronbd.com/public/api";
 // Token save 
 //  axios.defaults.baseURL = "https://api.aloronbd.com/public/api";
 axios.defaults.headers.common['Authorization'] = 'Bearer '+localStorage.getItem("TOKEN_KEY");
