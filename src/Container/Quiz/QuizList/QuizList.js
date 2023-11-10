28c0ba5767e2a2e7acfa6289082b4524e5a99aa7
raihanmiraj <rsnmiraj@gmail.com>
2023-11-10 16:29:04 +0600

    Update QuizList.js

--- src/Container/Quiz/QuizList/QuizList.js
+++ src/Container/Quiz/QuizList/QuizList.js
@@ -38,28 +38,27 @@ this.setState({
          var img = imgdata.hasOwnProperty('thumb')?imgdata.thumb:(imgdata.hasOwnProperty('medium')?imgdata.medium:imgdata.image)
              return (
               
-                <div key={QuizMeta[key].id} className="group relative">
-                <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+                <Link to={'/quiz/'+QuizMeta[key].id }  key={QuizMeta[key].id} className="group relative">
+                <div className="w-full  h-72 md:h-40 bg-gray-200 aspect-video rounded-md overflow-hidden group-hover:opacity-75   lg:aspect-video hover:blur-lg cursor-pointer relative">
                   <img
                     src={img}
                     alt={img }
                     className="w-full h-full object-center object-cover lg:w-full lg:h-full"
                   />
+                    <p className="absolute bottom-0 bg-yellow-400 right-0 text-sm font-semibold text-black px-5 py-1 rounded-tl-md">{QuizMeta[key].subject_name}</p>
+                    <p className="absolute top-0 right-0 bg-blue-500 text-white px-4 py-1 text-sm rounded-bl-md">{QuizMeta[key].point}</p>
                 </div>
-                <div className="mt-4 flex justify-between">
+              
+                <div className="mt-2 flex justify-between overflow-hidden">
                   <div>
-                    <h3 className="text-sm text-gray-700">
-                      <Link to={'/quiz/'+QuizMeta[key].id }>
-                <span aria-hidden="true" className="absolute inset-0" />
-                        {QuizMeta[key].quiz_name}
-                      </Link>
-  
-                    </h3>
-                    <p className="mt-1 text-sm text-gray-500">{QuizMeta[key].point}</p>
+                  <h3 className="text-md text-black font-semibold truncate " >
+                       {QuizMeta[key].quiz_name}
+                      </h3>
+                   
                   </div>
-                  <p className="text-sm font-medium text-gray-900">{QuizMeta[key].subject_name}</p>
+                 
                 </div>
-              </div>
+              </Link>
             )
         })
 
